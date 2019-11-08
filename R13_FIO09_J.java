d4422651c86f5cacdd716bc2e3d4489c23de00a7
Stephen Faber <faber.stephen@gmail.com>
2019-11-07 22:55:38 -0500

    Compliant Version

--- R13_FIO09_J.java
+++ R13_FIO09_J.java
@@ -1,13 +1,24 @@
 // Rule 13. Do not rely on the write() method to output integers
          // outside the range 0 to 255
-// Unompliant version
+// Compliant version
+
+// Compile with "javac R13_FIO09_J.java"
+// Run with "java R13_FIO09_J 254"
+
 import java.util.*;
+import java.io.*;
 
 public class R13_FIO09_J{
 
-  public static void main(String[] args) {
-    // Any input value > 255 will result in unexpected output
-    System.out.write(Integer.valueOf(args[0]));
+  public static void main(String[] args) throws NumberFormatException, IOException {
+    // Perform range checking
+    int value = Integer.valueOf(args[0]);
+
+    if (value < 0 || value > 255) {
+      throw new ArithmeticException("Value is out of range");
+    }
+
+    System.out.write(value);
     System.out.flush();
   }
 
