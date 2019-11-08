ce6961c0bd9e682c0c2caa8d1ae92c0b18f11bdc
Stephen Faber <faber.stephen@gmail.com>
2019-11-07 21:04:21 -0500

    Create R00_IDS03_J.java

---
+++ R00_IDS03_J.java
@@ -0,0 +1,13 @@
+// Rule 00. Input Validation and Data Sanitization (IDS)
+// Vulnerable version
+import java.util.*;
+
+public class R00_IDS03_J{
+  public static void main(string args){
+    if (loginSuccessful) {
+      logger.severe("User login succeeded for: " + username);
+    } else {
+      logger.severe("User login failed for: " + username);
+    }
+  }
+}
