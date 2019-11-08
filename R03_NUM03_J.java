16053e492fc547cffc22d7fca39ad30cc6b52e65
Stephen Faber <faber.stephen@gmail.com>
2019-11-07 23:00:21 -0500

    Compliant Version

--- R03_NUM03_J.java
+++ R03_NUM03_J.java
@@ -1,8 +1,19 @@
 // Rule 03. Use integer types that can fully represent the
-         // possible range of unsigned data
-// Vulnerable version
+//          possible range of unsigned data
+// Compliant version
+import java.io.*;
 
-public static int getInteger(DataInputStream is) throws IOException {
-  return is.readInt();
+public class R03_NUM03_J {
+
+  public static void main(String[] args) {
+
+    DataInputStream dataIn = new DataInputStream(new
+            FileInputStream("test.txt"));
+
+    getInteger(dataIn);
+  }
+
+  public static long getInteger(DataInputStream is) throws IOException {
+    return is.readInt() & 0xFFFFFFFFL; // Mask with 32 one-bits
+  }
 }
-
