62d7552a7d0e1e9008da321ba23cd488a6c34397
Stephen Faber <faber.stephen@gmail.com>
2019-11-07 22:57:15 -0500

    Compliant Version

--- R07_ERR04_J.java
+++ R07_ERR04_J.java
@@ -1,5 +1,9 @@
 // Rule 07. Do not complete abruptly from a finally block
-// Uncompliant version
+// Compliant version
+
+// Compile with "javac R07_ERR04_J.java"
+// Run with "java R07_ERR04_J"
+
 import java.util.*;
 
 public class R07_ERR04_J{
@@ -15,7 +19,8 @@ public class R07_ERR04_J{
       throw new IllegalStateException();
     } finally {
       System.out.println("logic done");
-      return true;
+
+      // Remove return statement
     }
   }
 
